7321e891f66c6ea23f1a9e7243919514399f256e
OriHero <aka.orihero@gmail.com>
2019-12-24 11:58:42 +0500

    MakeOrder

--- App.js
+++ App.js
@@ -3,73 +3,72 @@
  * @flow
  */
 
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Text, StatusBar, ActivityIndicator} from 'react-native';
-import AppNavigator from './navigation/AppNavigator';
 import {ApolloProvider} from '@apollo/react-hooks';
-import strings from './localization/strings';
-import {AsyncStorage} from 'react-native';
-import {Provider, connect} from 'react-redux';
-import store from './store/configureStore';
-
-import Header from './components/Header';
-import {userLoaded} from './actions/userActions';
-import {createHttpLink} from 'apollo-link-http';
-import {setContext} from 'apollo-link-context';
 import ApolloClient from 'apollo-boost';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, AsyncStorage, StyleSheet, View} from 'react-native';
+import {Provider} from 'react-redux';
+import {userLoaded} from './actions/userActions';
+import strings from './localization/strings';
+import AppNavigator from './navigation/AppNavigator';
+import store from './store/configureStore';
+import typeDefs from './graphql/typeDefs';
 
 const App: () => React$Node = () => {
+  let [loading, setLoading] = useState(true);
+  let [client, setClient] = useState(
+    new ApolloClient({
+      uri: 'https://yuz1.herokuapp.com/graphql',
+    }),
+  );
 
-    let [loading, setLoading] = useState(true);
-    let [client, setClient] = useState(new ApolloClient({
-            uri: 'https://yuz1.herokuapp.com/graphql',
+  let init = async () => {
+    let token = await AsyncStorage.getItem('token');
+    if (token) {
+      store.dispatch(userLoaded(token));
+      console.warn(store);
+      setClient(
+        new ApolloClient({
+          // uri: 'https://39990dea.ngrok.io/graphql',
+          uri: 'https://yuz1.herokuapp.com/graphql',
+          request: operation => {
+            operation.setContext({
+              headers: {
+                authorization: token ? token : '',
+              },
+            });
+          },
+          typeDefs,
         }),
-    );
-
-    let init = async () => {
-        let token = await AsyncStorage.getItem('token');
-        if (!!token) {
-            store.dispatch(userLoaded(token));
-            console.warn(store);
-            setClient(new ApolloClient({
-                // uri: 'https://39990dea.ngrok.io/graphql',
-                uri: 'https://yuz1.herokuapp.com/graphql',
-                request: (operation) => {
-                    operation.setContext({
-                        headers: {
-                            authorization: token ? token : '',
-                        },
-                    });
-                },
-            }));
-            console.log(token);
-        }
-        setLoading(false);
-    };
-    useEffect(init, []);
-
-
-    strings.setContent('uz');
-    if (loading) {
-        // return <View style={{flex: 1, backgroundColor: 'red'}}/>;
-        return <View style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}>
-            <ActivityIndicator size="large" color="#00ff00"/>
-        </View>;
+      );
+      console.log(token);
     }
+    setLoading(false);
+  };
+  useEffect(init, []);
 
+  strings.setContent('uz');
+  if (loading) {
+    // return <View style={{flex: 1, backgroundColor: 'red'}}/>;
     return (
-        <ApolloProvider client={client}>
-            <Provider store={store}>
-                <AppNavigator/>
-            </Provider>
-        </ApolloProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>
     );
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    </ApolloProvider>
+  );
 };
 
 const styles = StyleSheet.create({});
--- screens/Checkout/index.js
+++ screens/Checkout/index.js
@@ -1,239 +1,272 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, Image, FlatList, ActivityIndicator, Alert} from 'react-native';
-import colors from '../../constants/colors';
-import strings from '../../localization/strings';
+import {useLazyQuery, useMutation} from '@apollo/react-hooks';
+import React, {useEffect, useReducer} from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Touchable from '../../components/Touchable';
-import ProductInCheckout from '../../components/ProductInCheckout';
-import ProductCard from '../../components/ProductCard';
-import RoundedButton from '../../components/RoundedButton';
 import FilledButton from '../../components/FilledButton';
-import {useQuery, useLazyQuery} from '@apollo/react-hooks';
-import {GET_PRODUCT_DATA} from '../../graphql/requests';
-
+import ProductInCheckout from '../../components/ProductInCheckout';
+import Touchable from '../../components/Touchable';
+import colors from '../../constants/colors';
+import {GET_PRODUCT_DATA, ADD_TO_ORDER} from '../../graphql/requests';
+import strings from '../../localization/strings';
 
 const Checkout = ({navigation, item, modalOn}) => {
-    let [stockList, setStockList] = useState([]);
-    let [selectedType, setSelectedType] = useState(0);
-    let [totalCount, setTotalCount] = useState(0);
-    let [totalPrice, setTotalPrice] = useState(0);
-    let [orderList, setOrderList] = useState([]);
-    let [getProductData, {loading, data, error}] = useLazyQuery(GET_PRODUCT_DATA);
-
-
-    useEffect(() => {
-        if (!!item) {
-            getProductData({
-                variables: {
-                    product_id: item._id,
-                },
-            });
+  let [getProductData, {loading, data, error}] = useLazyQuery(GET_PRODUCT_DATA);
+  let [add, {data: addData}] = useMutation(ADD_TO_ORDER);
+  let reducer = (state, {type, id}) => {
+    switch (type) {
+      case 'INCREMENT': {
+        let element = state[id];
+        if (!element) {
+          return {...state, [id]: {qty: 1}};
         }
-        if (!!data) {
-            console.warn('getProductById');
-            console.warn(data.getProductById);
-            setStockList(data.getProductById.stock);
-            setOrderList(data.getProductById.stock);
+        return {...state, [id]: {qty: element.qty + 1}};
+      }
+      case 'DECREMENT': {
+        let element = state[id];
+        if (!element) {
+          return state;
         }
-    }, [data]);
+        return {...state, [id]: {qty: element.qty > 0 ? element.qty - 1 : 0}};
+      }
+      default:
+        return state;
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, {});
 
+  useEffect(() => {
+    if (item) {
+      getProductData({
+        variables: {
+          product_id: item._id,
+        },
+      });
+    }
+    if (addData) {
+      console.warn(addData);
+    }
+  }, [addData, data, getProductData, item]);
 
-
-    const alerter = () => {
-        Alert.alert(
-            'Alert Title',
-            'My Alert Msg',
-            [
-
-                {
-                    text: strings.cancel,
-                    onPress: () => console.warn('Cancel Pressed'),
-                    style: 'cancel',
-                },
-                {
-                    text: strings.ordering,
-                    onPress: () => {
-                        completeOrder();
-                        modalOn(false);
-                    },
-                    style: 'destructive',
-                },
-            ],
-            {cancelable: false},
-        );
-    };
-    const completeOrder = () => {
-        console.warn('ordered');
-    };
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <View style={styles.imageWrapper}>
-                    <Image style={{
-                        flex: 1,
-                        resizeMode: 'cover',
-                    }}
-                           source={{
-                               uri: item && item.images[0],
-                           }}/>
-                </View>
-                <View style={styles.textWrapper}>
-                    <Text style={styles.title}>{item && item.name}</Text>
-                    <Text style={styles.text}>{strings.minimumOrder} {item && item.min_order}</Text>
-                    <Text style={styles.price}>{item && item.price} {strings.priceUnit}</Text>
-                </View>
-                <View style={{
-                    marginRight: -10,
-                    marginTop: -5,
-                }}>
-                    <Touchable onPress={() => {
-                        modalOn(false);
-                    }}>
-                        <MaterialIcons name='close' size={30} color={colors.white}/>
-                    </Touchable>
-                </View>
-            </View>
-            <View style={styles.content}>
-                <View>
-                    <Text style={styles.contentTitle}>
-                        {strings.option}:
-                    </Text>
-                </View>
-                <View>
-                    {loading ? (
-                        <View style={{
-                            flex: 1,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}>
-                            <ActivityIndicator size="large" color="#00ff00"/>
-                        </View>) : (
-                        <FlatList
-                            style={{
-                                height: 300,
-                            }}
-                            data={stockList}
-                            keyExtractor={item => item._id}
-                            renderItem={({item}) => <ProductInCheckout
-                                item={item}
-                                setCount={setTotalCount}
-                                count={totalCount}
-                                setTypeCount={setSelectedType}
-                                typeCount={selectedType}
-                                setPrice={setTotalPrice}
-                                price={totalPrice}
-                                createOrderList={setOrderList}
-                                createdOrder={orderList}
-                                copyOfOrderList={stockList}
-                            />}
-
-                        />)}
-                </View>
-                <View style={styles.bottomWrapper}>
-                    <View style={styles.overall}>
-                        <Text style={{
-                            color: colors.green,
-                        }}>
-                            {selectedType}{' '}
-                        </Text>
-                        <Text>
-                            {strings.type},{'  '}
-                        </Text>
-                        <Text style={{
-                            color: colors.green,
-                        }}>
-                            {totalCount}{' '}
-                        </Text>
-                        <Text>
-                            {strings.piece}, {strings.totalPrice}:{'  '}
-                        </Text>
-                        <Text style={{
-                            color: colors.green,
-                        }}>
-                            {totalPrice}{' '}
-                        </Text>
-                        <Text>
-                            {strings.uzs}
-                        </Text>
-                    </View>
-                    <View style={styles.buttonWrapper}>
-                        <Touchable onPress={() => {
-                            console.warn('some');
-                            alerter();
-                        }}>
-                            <View>
-                                <FilledButton text={strings.ordering}/>
-                            </View>
-                        </Touchable>
-                    </View>
-                </View>
-            </View>
-        </View>
+  const alerter = () => {
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {
+          text: strings.cancel,
+          onPress: () => console.warn('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: strings.ordering,
+          onPress: () => {
+            completeOrder();
+            modalOn(false);
+          },
+          style: 'destructive',
+        },
+      ],
+      {cancelable: false},
     );
+  };
+  const completeOrder = () => {
+    let stock = Object.keys(state).map(key => {
+      let {qty} = state[key];
+      return {_id: key, qty};
+    });
+    add({variables: {input: {product_id: item._id, stock}}});
+  };
+
+  let totalCount = 0;
+  let totalPrice = 0;
+  if (Object.keys(state).length > 0) {
+    Object.keys(state).forEach(key => {
+      let el = state[key];
+      totalCount += el.qty;
+      totalPrice = el.qty * item.price;
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.imageWrapper}>
+          <Image
+            style={{
+              flex: 1,
+              resizeMode: 'cover',
+            }}
+            source={{
+              uri: item && item.images[0],
+            }}
+          />
+        </View>
+        <View style={styles.textWrapper}>
+          <Text style={styles.title}>{item && item.name}</Text>
+          <Text style={styles.text}>
+            {strings.minimumOrder} {item && item.min_order}
+          </Text>
+          <Text style={styles.price}>
+            {item && item.price} {strings.priceUnit}
+          </Text>
+        </View>
+        <View
+          style={{
+            marginRight: -10,
+            marginTop: -5,
+          }}>
+          <Touchable
+            onPress={() => {
+              modalOn(false);
+            }}>
+            <MaterialIcons name="close" size={30} color={colors.white} />
+          </Touchable>
+        </View>
+      </View>
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.contentTitle}>{strings.option}:</Text>
+        </View>
+        <View>
+          {loading ? (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <ActivityIndicator size="large" color="#00ff00" />
+            </View>
+          ) : (
+            <FlatList
+              style={{
+                height: 300,
+              }}
+              data={data && data.getProductById.stock}
+              keyExtractor={el => el._id}
+              renderItem={({item: el}) => (
+                <ProductInCheckout
+                  item={el}
+                  state={state}
+                  dispatch={dispatch}
+                />
+              )}
+            />
+          )}
+        </View>
+        <View style={styles.bottomWrapper}>
+          <View style={styles.overall}>
+            <Text
+              style={{
+                color: colors.green,
+              }}>
+              {Object.keys(state).length}{' '}
+            </Text>
+            <Text>
+              {strings.type},{'  '}
+            </Text>
+            <Text
+              style={{
+                color: colors.green,
+              }}>
+              {totalCount}{' '}
+            </Text>
+            <Text>
+              {strings.piece}, {strings.totalPrice}:{'  '}
+            </Text>
+            <Text
+              style={{
+                color: colors.green,
+              }}>
+              {totalPrice}{' '}
+            </Text>
+            <Text>{strings.uzs}</Text>
+          </View>
+          <View style={styles.buttonWrapper}>
+            <Touchable
+              onPress={() => {
+                console.warn('some');
+                alerter();
+              }}>
+              <View>
+                <FilledButton text={strings.ordering} />
+              </View>
+            </Touchable>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        width: 480,
-        backgroundColor: colors.white,
-        // borderBottomRightRadius: 40,
-    },
-    header: {
-        flexDirection: 'row',
-        paddingVertical: 15,
-        paddingHorizontal: 20,
-        backgroundColor: colors.green,
-        // borderBottomRightRadius: 40,
-    },
-    imageWrapper: {
-        overflow: 'hidden',
-        height: 85,
-        width: 85,
-        // borderRadius: 10,
-    },
-    textWrapper: {
-        paddingHorizontal: 20,
-        flex: 1,
-        justifyContent: 'center',
-    },
-    title: {
-        color: colors.white,
-        fontSize: 17,
-        fontWeight: 'bold',
-        marginBottom: 5,
-    },
-    text: {
-        marginBottom: 7,
-    },
-    price: {
-        marginBottom: 5,
-        fontSize: 15,
-        fontWeight: 'bold',
-    },
-    content: {
-        paddingVertical: 15,
-        paddingHorizontal: 20,
-        paddingBottom: 30,
-    },
-    contentTitle: {
-        fontSize: 20,
-        color: colors.black,
-        marginBottom: 15,
-        fontWeight: 'bold',
-    },
-    bottomWrapper: {
-        alignItems: 'center',
-    },
-    overall: {
-        flexDirection: 'row',
-        paddingVertical: 20,
-        paddingTop: 60,
-    },
-    buttonWrapper: {
-        paddingHorizontal: 100,
-    },
+  container: {
+    width: 480,
+    backgroundColor: colors.white,
+    // borderBottomRightRadius: 40,
+  },
+  header: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    backgroundColor: colors.green,
+    // borderBottomRightRadius: 40,
+  },
+  imageWrapper: {
+    overflow: 'hidden',
+    height: 85,
+    width: 85,
+    // borderRadius: 10,
+  },
+  textWrapper: {
+    paddingHorizontal: 20,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    color: colors.white,
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  text: {
+    marginBottom: 7,
+  },
+  price: {
+    marginBottom: 5,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  content: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    paddingBottom: 30,
+  },
+  contentTitle: {
+    fontSize: 20,
+    color: colors.black,
+    marginBottom: 15,
+    fontWeight: 'bold',
+  },
+  bottomWrapper: {
+    alignItems: 'center',
+  },
+  overall: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+    paddingTop: 60,
+  },
+  buttonWrapper: {
+    paddingHorizontal: 100,
+  },
 });
 
 export default Checkout;
